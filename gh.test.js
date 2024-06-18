4561760d4a5cd2b83e88f0024cb4449082e07e0f
migbaranes <migbaranes@gmail.com>
2024-06-18 12:21:28 +0300

    first_commit

---
+++ gh.test.js
@@ -0,0 +1,37 @@
+let page;
+
+beforeEach(async () => {
+  page = await browser.newPage();
+  await page.goto("https://github.com/team");
+});
+
+afterEach(() => {
+  page.close();
+});
+
+describe("Github page tests", () => {
+  test("The h1 header content'", async () => {
+    jest.setTimeout(5000);
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub');
+  });
+
+  test("The first link attribute", async () => {
+    jest.setTimeout(5000);
+    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    expect(actual).toEqual("#start-of-content");
+  });
+
+  test("The page contains Sign in button", async () => {
+    jest.setTimeout(5000);
+    const btnSelector = ".btn-large-mktg.btn-mktg";
+    await page.waitForSelector(btnSelector, {
+      visible: true,
+    });
+    const actual = await page.$eval(btnSelector, link => link.textContent);
+    expect(actual).toContain("Get started with Team")
+  });
+});
